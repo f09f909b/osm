996570fdc9912d5221cf3275ae6e8bc6a13d646d
Moises Sanchez <moijsanchez@gmail.com>
2021-12-28 08:53:36 +0000

    Create death zone script + restart ability

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,6 +37,11 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
     public void Restart()
     {
         
